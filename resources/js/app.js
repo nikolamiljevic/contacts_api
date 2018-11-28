188509e3917742a7914df980ad7754679d79bd3b
Nikola <milje18@gmail.com>
2018-11-28 16:14:33 +0100

    contact list

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+let axios = require('axios');
+
+Vue.component('contacts', require('./components/Contacts.vue'));
+
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="csrf-token" content="{{csrf_token()}}">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Contact store</title>
+</head>
+<body>
+    <div class="container">
+        <div id="app">
+             <contacts>
+            </contacts>
+        </div>
+    </div>
+<script src="/js/app.js"></script>
+</body>
+</html>
